50903e9b6018c89a682b1f66f0b4b8cc49166395
Mukul Pathania <mukulpathania96@gmail.com>
2020-01-03 19:29:40 +0530

    Add a README file and support for Jinja2 templates
    Added a README file for the viewers(if any ..) of this repository to
    help them understand the purpose of this software.
    Added 2 Jinja2 templates in the app. One is the the generic base
    template that most of the pages in the app will inherit from and another
    is for the view function "index".
    Both the templates are dynamic in nature due to the control structures in them
    that helps to load pages differently for different data that is provided to templates.

---
+++ README.md
@@ -0,0 +1,12 @@
+First ever project by me and my friends.
+========================================
+
+This Repository contains the software that will be released on web 
+(if implemented by us properly) to help us and our classmates share 
+our assignments with our class.
+
+We have just started in the world of programming so sorry for all the 
+shortcomings and bad practices that may have crept into this work 
+(We probably don't even know what is bad).
+
+If you have any suggestions for us, they are more than welcome. 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index ():
-    return "Hello, World!"
+    user = {"username":"Mukul"}
+    posts = [{"author" : {"username" : "Sunita"},
+            "body" : "Beautiful day in Kharghar"} ,
+            { "author" : {"username" : "Vishav"} ,
+                "body" : "That child was way too cute."}
+            ]
+    return render_template("index.html",
+            title = "Home", 
+            user = user,
+            posts=posts)
